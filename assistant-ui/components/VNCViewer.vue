acccdf4931d295f26fca6c7e8e0251af19364084
Alif <alif@smartm2m.co.kr>
2025-08-08 08:11:19 +0700

    fix hydration vncviewer

--- assistant-ui/components/VNCViewer.vue
+++ assistant-ui/components/VNCViewer.vue
@@ -1,8 +1,23 @@
 <template>
-    <iframe :src="vmUrl" frameborder="0" title="Example iframe" class="h-full w-full overflow-scroll" />
+  <div class="h-full w-full">
+    <iframe 
+      v-if="shouldRenderIframe"
+      :src="vmUrl" 
+      frameborder="0" 
+      title="Example iframe" 
+      class="h-full w-full overflow-scroll" 
+    />
+    <div 
+      v-else 
+      class="h-full w-full flex items-center justify-center"
+    >
+      Loading...
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 const config = useRuntimeConfig();
-const vmUrl = config.vmUrl;
+const vmUrl = config.public?.vmUrl || ''; // Make sure this is in public config
+const shouldRenderIframe = computed(() => !!vmUrl);
 </script>
